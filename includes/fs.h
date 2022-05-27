cf96232f80e3067a300266d969c0ac4420545f21
vinicius.yoshioka <101149767+vinicius-yoshioka@users.noreply.github.com>
2022-05-27 20:27:55 -0300

    Adicionado comentários para fs.h

--- includes/fs.h
+++ includes/fs.h
@@ -2,9 +2,21 @@
 #include "../includes/listaString.h"
 #endif
 
+// Definição para verificar se este header já foi importado ou não
 #define _FS_H 1
 
+// Define o tamanho da string que armazena caminhos completos de arquivos/pastas
 #define TAMANHO_CAMINHO 4096
+
+// Define o tamanho da string que armazena o nome de arquivos
 #define TAMANHO_NOME_ARQUIVO 256
 
+/**
+ * Lista, recursivamente, o conteúdo da pasta dada pelo parâmetro 'caminho',
+ * os arquivos são adicionados à lista dada pelo parâmetro 'arquivos' e
+ * as pastar são varridas com recursão
+ *
+ * @param arquivos Ponteiro de uma ListaString que armazena caminhos de arquivos
+ * @param caminho Caminho da pasta a ser lida
+ */
 void pegarConteudo(ListaString *arquivos, char caminho[]);
