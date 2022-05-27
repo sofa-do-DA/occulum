3fc7c76f2165a6a36f03eff03f4107f1de2353b2
vinicius.yoshioka <101149767+vinicius-yoshioka@users.noreply.github.com>
2022-05-27 20:27:23 -0300

    Comentários movdos para arquivo .h

--- includes/cmp.h
+++ includes/cmp.h
@@ -1,4 +1,16 @@
 
+// Definição para verificar se este header já foi importado ou não
 #define _CMP_H 1
 
+/**
+ * Compara duas strings
+ *
+ * @param string1 string primária da comparação
+ * @param string2 string a ser comparada com a string1
+ *
+ * @return Inteiro indicando a igualdade.
+ * Quando 0 é retornado, as duas strings são iguais;
+ * Quando -1 é retornado, a string1 é menor que a string2;
+ * Quando 1 é retornado, a string1 é maior que a string2;
+ */
 int string_cmp(char string1[], char string2[]);
--- libs/cmp.c
+++ libs/cmp.c
@@ -6,17 +6,6 @@
 #include "../includes/cmp.h"
 #endif
 
-/**
- * Compara duas strings
- *
- * @param string1 string primária da comparação
- * @param string2 string a ser comparada com a string1
- *
- * @return Inteiro indicando a igualdade.
- * Quando 0 é retornado, as duas strings são iguais;
- * Quando -1 é retornado, a string1 é menor que a string2;
- * Quando 1 é retornado, a string1 é maior que a string2;
- */
 int string_cmp(char string1[], char string2[])
 {
     int i = 0;
