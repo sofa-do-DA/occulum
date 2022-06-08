a5c138cc6148df2baa8dc76d6af41ff213a75e73
vinicius.yoshioka <101149767+vinicius-yoshioka@users.noreply.github.com>
2022-06-08 19:45:11 -0300

    Adicionado texto da função usage

--- libs/usage.c
+++ libs/usage.c
@@ -3,4 +3,9 @@
 void usage()
 {
     printf("\n");
+    printf("Uso: Occulum\n");
+    printf("\n");
+    printf("Ao executar o programa, passe o termo de busca.\n");
+    printf("O programa irá listar todos os arquivos contidos na pasta 'conteudo' e pesquisar um arquivo com mesmo nome do termo dado.\n");
+    printf("\n");
 }
