b8b5e1f3225ff757104241331d9ac2768a9d5a25
vinicius.yoshioka <101149767+vinicius-yoshioka@users.noreply.github.com>
2022-06-14 19:57:30 -0300

    Melhorada funcao de comparacao de strings

--- libs/cmp.c
+++ libs/cmp.c
@@ -6,21 +6,27 @@
 #include "cmp.h"
 #endif
 
-void lowerString(char string[])
-{
-    int tamanhoString = strlen(string);
-    for (int i = 0; i < tamanhoString; i++)
-    {
-        string[i] = tolower(string[i]);
-    }
-}
-
 int string_cmp(char string1[], char string2[])
 {
-    // Converte as string para minúsculo
-    lowerString(string1);
-    lowerString(string2);
+    int i;
+    int tamanhoString = strlen(string1);
 
-    // Compara as duas strings
-    return strcmp(string1, string2);
+    for (i = 0; i < tamanhoString; i++)
+    {
+        if (tolower(string1[i]) < tolower(string2[i]))
+        {
+            return -1;
+        }
+        else if (tolower(string1[i]) > tolower(string2[i]))
+        {
+            return 1;
+        }
+    }
+
+    if (tolower(string1[i]) < tolower(string2[i]))
+    {
+        return -1;
+    }
+
+    return 0;
 }
